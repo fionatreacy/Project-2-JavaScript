1e55b47dab907f94566829fe89ec0f9f29986284
fionatreacy <fionatreacy00@gmail.com>
2022-03-07 16:16:17 +0000

    rock paper scissors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,116 @@
-document.addEventListener('DOMContentLoaded', () => {
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
 
-    // card options //
-    const cardArray = [{
-        name: 'anglerFish',
-        img: 'images/angler-fish.png'
-    } {
-        name: 'crab',
-        img: 'images/crab.png'
-    } {
-        name: 'fish',
-        img: 'images/fish.png'
-    } {
-        name: 'jellyfish',
-        img: 'images/jellyfish.png'
-    } {
-        name: 'octopus',
-        img: 'images/octopus.png'
-    } {
-        name: 'squid',
-        img: 'images/squid.png'
-    }]
+  //Start the Game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
 
-    cardArray.sort(() => 0.5 - Math.random())
-    // gameboard //
-    const grid = document.querySelector('.grid')
-    const resultDisplay = document.querySelector('#result')
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
 
-    var cardsChosen = []
-    var cardsChosenId = []
-    var cardsWon = []
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
 
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            var = document.createElement('img')
-            card.setAttribute('src', 'assets/images/blank.png')
-            card.setAttribute('data-id', i)
-            card.addEventListener('click', flipcard)
-            grid.appendChild(card)
-        }
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `assets/images/${this.textContent}.png`;
+          computerHand.src = `assets/images/${computerChoice}.png`;
+        }, 2000);
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "Tie!";
+      return;
     }
-
-    // flip cards //
-    function flipCard() {
-        var cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenId.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500)
-        } else {
-            cards[optionOneId].setAttribute('src', 'assets/images/blank.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/blank.png')
-            alert('Not a match, try again')
-        }
-        cardsChosen = []
-        cardsChosenId = []
-        resultDisplay.textContent = cardsWon.length
-        if (cardsWon.length === cardArray.length/2) {
-            resultDisplay.textContent = "Congatulations you've won!"
-        }
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins!";
+        cScore++;
+        updateScore();
+        return;
+      }
     }
-
-
-    // check matches //
-    function checkForMatch() {
-        var cards = document.querySelectorAll('img')
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-        if (cardsChosen[0] === cardsChosen[1]) {
-            alert("You've found a match!")
-            cardsWon.push(cardsChosen)
-        }
+    //Check for Paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
     }
+    //Check for Scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
 
+  //Is call all the inner function
+  startGame();
+  playMatch();
+};
 
-})
+//start the game function
+game();
