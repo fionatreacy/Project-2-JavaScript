d94a08613f25341955a56a069e1a662e31041853
fionatreacy <fionatreacy00@gmail.com>
2022-03-07 13:58:00 +0000

    add card array to js file

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // card options //
+const cardArray = [
+    {
+        name: 'anglerFish', 
+        img: 'images/angler-fish.png'
+    }
+    {
+        name: 'crab', 
+        img: 'images/crab.png'
+    }
+    {
+        name: 'fish', 
+        img: 'images/fish.png'
+    }
+    {
+        name: 'jellyfish', 
+        img: 'images/jellyfish.png'
+    }
+    {
+        name: 'octopus', 
+        img: 'images/octopus.png'
+    }
+    {
+        name: 'squid', 
+        img: 'images/squid.png'
+    }
+]
+})
